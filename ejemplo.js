916233bf84ec16c7b9aa16a6479377a3620b41bd
Ana Isabel Zapata Gutiérrez <anaisabel.1045@gmail.com>
2017-07-12 09:53:03 -0500

    Ejemplo de javascript y confirm

---
+++ ejemplo.js
@@ -0,0 +1,9 @@
+alert("Hola mundo");
+if(confirm("Estas contento en clase?")){
+	//lo que hace si se cumple la condición
+	alert("Genial, me alegra");
+}else{
+	//lo que hace si no se cumple la condicion 
+	alert("A ok, entonces vete");
+}
+
