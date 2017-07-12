50585cc1a31465dcd746997c0a0d9ec516578211
Ana Isabel Zapata Gutiérrez <anaisabel.1045@gmail.com>
2017-07-12 10:37:20 -0500

    alargue el mensaje del alert verdadero

--- ejemplo.js
+++ ejemplo.js
@@ -1,7 +1,7 @@
 alert("Hola mundo");
 if(confirm("Estas contento en clase?")){
 	//lo que hace si se cumple la condición
-	alert("Genial, me alegra");
+	alert("Genial, me alegra, todo bien, empieza a trabajar");
 }else{
 	//lo que hace si no se cumple la condicion 
 	alert("A ok, entonces adios");
